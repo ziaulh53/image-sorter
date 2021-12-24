bed805114110f20342d09281f3231a607a96a4fe
ziaulh53 <ziaul.siu@gmail.com>
2021-12-25 02:16:54 +0600

    admin done

--- src/config/_api.js
+++ src/config/_api.js
@@ -1,5 +1,6 @@
-export const GRAPHQL_URL = "https://greatgable.fra1.cdn.digitaloceanspaces.com/"
-// "https://image-sorter-api.herokuapp.com/";
+export const GRAPHQL_URL = 
+// "https://greatgable.fra1.cdn.digitaloceanspaces.com/"
+"https://image-sorter-api.herokuapp.com/";
 //  "http://192.168.0.9:4000/graphql";
 //  "http://localhost:7000/graphql"
 // process.env.REACT_APP_GRAPHQL_ENDPOINT
--- src/util/helper.js
+++ src/util/helper.js
@@ -1,4 +1,4 @@
-const endpoint = process.env.REACT_APP_IMAGE_URL || "https://assets.chillfitrave.com/";
+const endpoint = process.env.REACT_APP_IMAGE_URL || "https://greatgable.fra1.cdn.digitaloceanspaces.com/";
 const noImage = "/images/noimage.png";
 export const getImage = image => {
   if (!image) return noImage;
